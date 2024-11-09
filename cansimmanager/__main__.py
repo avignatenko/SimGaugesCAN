da0e5231deb1b7db0a5e96348f9f1495739713e1
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2024-11-09 08:38:27 -0800

    some code to run as script

--- cansimmanager/__main__.py
+++ cansimmanager/__main__.py
@@ -1,4 +1,20 @@
 print(f'Invoking __main__.py for {__name__}')
 
+from .common import *
+
 if __name__ == "__main__":
     print("main script will be here in future")
+
+    config = read_config()
+    bus = can.interface.Bus(
+        interface="slcan",
+        channel=config["channel"],
+        ttyBaudrate=config["ttyBaudrate"],
+        bitrate=1000000,
+    )
+
+    # enable voltage
+    send_command(bus, 1, 31, 0, 1, make_payload_byte(12))
+    send_command(bus, 1, 31, 0, 2, make_payload_byte(1))
+
+    bus.shutdown()
