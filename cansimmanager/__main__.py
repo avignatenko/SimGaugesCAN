b61c778624fc12929c99621251b9bba00bf5c592
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2024-11-17 12:33:50 -0800

    refactor

--- cansimmanager/__main__.py
+++ cansimmanager/__main__.py
@@ -11,7 +11,7 @@ from .arrowiiiturbodevices import arrowiiiturbodevices
 logger = logging.getLogger(__name__)
 
 
-async def main() -> None:
+async def main_loop() -> None:
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
     logger.info("Reading config")
@@ -26,31 +26,39 @@ async def main() -> None:
         try:
             uri = config["simAddr"]
             await sim.connect(uri)
+            logger.info("Sim connected")
             break
         except (OSError, TimeoutError) as e:
             logger.error("Sim connection error: %s, reconnecting", e)
             continue
 
     logger.info("CAN bus init")
-
-    try:
-        can = Can()
-        await can.connect(config["channel"], config["ttyBaudrate"])
-        can.
-    except Exception as e:
-        logger.error("CAN error: %s", e)
-        exit(-1)
+    can = Can()
+    while True:
+        try:
+            await can.connect(config["channel"], config["ttyBaudrate"])
+            logger.info("CAN connected")
+            break
+        except Exception as e:
+            logger.error("CAN error: %s, reconnecting", e)
+            asyncio.sleep(2.0)  # CAN doesnt have connection timeout, so we'll wait here
+            continue
 
     # subsribe devices
     logger.info("Registering devices")
     arrowiiiturbodevices.register(sim, can)
+
+    logger.info("Devices init")
     await arrowiiiturbodevices.init()
 
     # run can receiver and websockets receiver async
-    logger.info("Starting can and websockets listeners")
+    logger.info("Starting main loop")
     async with asyncio.TaskGroup() as tg:
         task1 = tg.create_task(can.run())
         task2 = tg.create_task(sim.run())
 
+def main():
+    asyncio.run(main_loop())
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
