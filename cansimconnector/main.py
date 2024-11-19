b7e41eade8cefc0e8c5ec903c333c6fc9b001327
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2024-11-19 13:33:40 -0800

    refactoring

--- cansimconnector/main.py
+++ cansimconnector/main.py
@@ -7,15 +7,7 @@ from . import arrowiiiturbodevices, cansimlib
 logger = logging.getLogger(__name__)
 
 
-async def main_loop() -> None:
-    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-
-    logger.info("Reading config")
-    config = cansimlib.read_config()
-
-    # setup main objects
-    logger.info("Making main objects")
-
+async def connect_sim(config) -> cansimlib.XPlaneClient:
     logger.info("Sim connection init")
     sim = cansimlib.XPlaneClient()
     while True:
@@ -27,7 +19,10 @@ async def main_loop() -> None:
         except (OSError, TimeoutError) as e:
             logger.error("Sim connection error: %s, reconnecting", e)
             continue
+    return sim
 
+
+async def connect_can(config) -> cansimlib.CANClient:
     logger.info("CAN bus init")
     can = cansimlib.CANClient()
     while True:
@@ -39,6 +34,20 @@ async def main_loop() -> None:
             logger.error("CAN error: %s, reconnecting", e)
             asyncio.sleep(2.0)  # CAN doesnt have connection timeout, so we'll wait here
             continue
+    return can
+
+
+async def main_loop() -> None:
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+    logger.info("Reading config")
+    config = cansimlib.read_config()
+
+    # setup main objects
+    logger.info("Making main objects")
+
+    sim = await connect_sim(config)
+    can = await connect_can(config)
 
     # subsribe devices
     logger.info("Registering devices")
