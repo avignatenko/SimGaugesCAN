ec2a4073609de0654962b083bc6f5c050c8f66b9
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2025-01-12 01:25:31 +0300

    added command to engage/disengage CL

--- src/cansimconnector/arrowiiiturbodevices/AI-Plugins-cl.lua
+++ src/cansimconnector/arrowiiiturbodevices/AI-Plugins-cl.lua
@@ -1,3 +1,10 @@
 -- NoChange = 0, Engage = 1, Disengage = 2
 cl_engage = create_dataref_table("ai/cockpit/devices/cl_engage", "Int")
 cl_engage[0] = 0
+
+function engage_cl(value)
+    cl_engage[0] = value
+  end
+
+create_command("ai/cockpit/devices/cl_engage_on", "AI: Engage CL", "engage_cl(1)", "", "")
+create_command("ai/cockpit/devices/cl_engage_off", "AI: Disengage CL", "engage_cl(2)", "", "")
