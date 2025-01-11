4e81a93fa04b29c2d95aeaa660aaa6d629d6ffbf
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2025-01-12 00:35:58 +0300

    updated lua stuff

---
+++ src/cansimconnector/arrowiiiturbodevices/AI-Plugins-cl.lua
@@ -0,0 +1,3 @@
+-- NoChange = 0, Engage = 1, Disengage = 2
+cl_engage = create_dataref_table("ai/cockpit/devices/cl_engage", "Int")
+cl_engage[0] = 0
--- src/cansimconnector/arrowiiiturbodevices/AI-Plugon-ap-pitch.lua
+++ src/cansimconnector/arrowiiiturbodevices/AI-Plugon-ap-pitch.lua
@@ -1,14 +1,9 @@
 -- for stec-30 autopilot gauge
 
 -- 0 - off, 1 - on
-ap_engaged = create_dataref_table("ai/cockpit/gauges/ap/engaged", "Int")
+ap_engaged = create_dataref_table("ai/cockpit/gauges/ap/pitch_engaged", "Int")
 ap_engaged[0] = 0
 
 -- 0 - no warning, 1 - pitch up, 2 - pitch up more, 3 - pitch down, 4 - pitch down more
 pitch_trim_warning = create_dataref_table("ai/cockpit/gauges/ap/pitch_trim_warning", "Int")
 pitch_trim_warning[0] = 0
--- for CL control
-
--- NoChange = 0, Engage = 1, Disengage = 2
-cl_engage = create_dataref_table("ai/cockpit/devices/cl_engage", "Int")
-cl_engage[0] = 0
